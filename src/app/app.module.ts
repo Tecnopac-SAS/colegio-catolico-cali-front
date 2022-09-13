d385d4a8c5d8e7817793a80ce3f7a5f7f371f473
alvarotecnopac <alvarosantacruz@tecnopac.com.co>
2022-09-13 11:25:17 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { UserManagerComponent } from './component/user-manager/user-manager.component';
+import { CreateUserComponent } from './component/user-manager/create-user/create-user.component';
+import { IndexUserComponent } from './component/user-manager/index-user/index-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    UserManagerComponent,
+    CreateUserComponent,
+    IndexUserComponent
   ],
   imports: [
     BrowserModule,
