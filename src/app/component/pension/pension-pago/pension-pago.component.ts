958a42c088e464f71b646be54a9ae8a4f5a900fd
josRod128 <jose12836@gmail.com>
2023-01-25 22:52:32 -0700

    seleccion meses

--- src/app/component/pension/pension-pago/pension-pago.component.ts
+++ src/app/component/pension/pension-pago/pension-pago.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PensionPagoService } from 'src/app/services/pension-pago.service';
 import { TuitionService } from 'src/app/services/tuition.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pension-pago',
@@ -9,12 +10,14 @@ import { TuitionService } from 'src/app/services/tuition.service';
 })
 export class PensionPagoComponent implements OnInit {
   navTitle="Pago de pension"
-  public pensionTotal:any
+  public pensionTotal=0
   public pensionesList:any
+  public pensionesListSelect:any
   public mesesArr = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
   public matriculaPagada:any
 
   constructor(private pensionService:PensionPagoService,private matriculaService:TuitionService) { 
+    this.pensionesListSelect=[]
   }
   
   
@@ -39,5 +42,39 @@ export class PensionPagoComponent implements OnInit {
   parseMes(fecha:any){
     const subFecha = Number(fecha.substring(5,7))
     return this.mesesArr[subFecha-1]
+  }
+  checkPendientes(fecha:any,$event:any){
+    this.pensionTotal=0
+    this.pensionesListSelect=[]
+    let text=''
+    Object.keys(this.pensionesList).forEach(key => {
+      let checkBox= document.getElementById('check'+this.pensionesList[key].id) as HTMLInputElement
+      if((new Date(fecha) > new Date(this.pensionesList[key].fechaPago))){
+        if (!checkBox.checked) {
+          if (text=='') {
+            text='Favor de seleccionar '+this.parseMes(this.pensionesList[key].fechaPago)
+          }else{
+            text+=', '+this.parseMes(this.pensionesList[key].fechaPago)
+          }
+        }else{
+          this.pensionTotal+=this.pensionesList[key].valor
+          this.pensionesListSelect.push({id:this.pensionesList[key].id,valor:this.pensionesList[key].valor})
+        }
+      }else{
+        if (checkBox.checked) {
+          this.pensionTotal+=this.pensionesList[key].valor
+          this.pensionesListSelect.push({id:this.pensionesList[key].id,valor:this.pensionesList[key].valor})
+        }
+      }
+    });
+    if (text!='') {
+      $event.currentTarget.checked=false
+      text+=' antes de seleccionar esta pensión'
+      Swal.fire(
+        text,
+        '',
+        'info'
+      )
+    }
   }
 }
