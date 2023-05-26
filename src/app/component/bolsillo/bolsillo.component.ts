2106b8a39d52ab552430e04f3f0c0ede93020d4d
AndresBojaca <andresbojaca742@gmail.com>
2023-05-26 16:27:53 -0500

    feat(*): Bolsillo y AvalPay

--- src/app/component/bolsillo/bolsillo.component.ts
+++ src/app/component/bolsillo/bolsillo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BolsilloService } from 'src/app/services/bolsillo.service';
+import { AvalPayService } from 'src/app/services/avalpay.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import Swal from 'sweetalert2';
 
@@ -14,24 +17,35 @@ export class BolsilloComponent implements OnInit {
   public bolsillo: any
   formValue!: FormGroup 
   public disableButton: boolean = true;
+  descPagoAvalPay: string;
+  pmtId: any;
+  lsBolsillo: any;
+  numberInput: any;
 
   constructor(
     private formBuilder:FormBuilder,
-    private bolsilloService:BolsilloService
+    private bolsilloService:BolsilloService,
+    private AvalPayService:AvalPayService,
+    private route: ActivatedRoute,
+    private router:Router,
     ) { 
       this.bolsillo =  localStorage.getItem('bolsillo')
+      this.descPagoAvalPay = '';
+      this.numberInput = 0;
       this.formValue = this.formBuilder.group({
         cant: ['', [Validators.required, Validators.min(0), Validators.max(5000)]]
       });
   }
 
   ngOnInit(): void {
+    this.validateTransactions()
     this.fieldCapture()
     this.bolsillo = localStorage.getItem('bolsillo')
   }
 
   validateNumber(event: any) {
     let numberInput: any = event.target;
+    this.numberInput = numberInput.value;
     let number = parseFloat(numberInput.value);
     let minValue = 1;
     let maxValue = 5000000;
@@ -60,6 +74,78 @@ export class BolsilloComponent implements OnInit {
 
     });
   }
+
+
+  validateTransactions(){
+    //Obtenemos el id de la transaccion
+    // this.route.queryParams.subscribe(params => {
+    //   if(params['pmtId']){
+    //     this.pmtId = params['pmtId'];
+    //     this.AvalPayService.makePaymentStatus(this.pmtId).subscribe(response =>{
+
+    //       let trnStatus = response.message.InvoicePmtInfo.PmtStatus.StatusDesc;
+    //       let lstransactionStatus:any = localStorage.getItem('bolsillo-transaction-status');
+    //       this.lsBolsillo = JSON.parse(lstransactionStatus);
+          
+    //       if(trnStatus == 'Aprobada' && this.lsBolsillo.trnStatus != true ){
+    //         let datos = {pensiones:this.lsPensionesListSelect.months}
+    //         this.pensionService.pagoPension(datos,'AvalPay').subscribe(response=>{},error=>{});
+    //         Swal.fire(
+    //           'Transaccion Exitosa!',
+    //           `#${this.pmtId} El pago de tu pensión fue ${trnStatus}`,
+    //           'success'
+    //         ).then((result) => {
+    //             let trnNewStatus = this.lsPensionesListSelect.trnStatus = true;
+    //             localStorage.setItem('transaction-status', JSON.stringify(this.lsPensionesListSelect));
+    //             setTimeout(() => {
+    //               this.router.navigate(['/pago-pension']);
+    //               this.getListPensiones()
+    //             }, 1000);
+    //         })
+    //       }else{
+    //         Swal.fire(
+    //           'Hubo un error en la transacción!',
+    //           `#${this.pmtId} El pago de tu pensión fue ${trnStatus}`,
+    //           'error'
+    //         ).then((result) => {
+    //           setTimeout(() => {
+    //             this.router.navigate(['/pago-pension']);
+    //           }, 1000);
+    //       })
+    //       }
+
+    //     });
+    //   }
+    // });
+  }
+
+
+
+  recargarBolsilloAvalPay(amount: any, invoiceType = 1, desc = 'RECARGA BOLSILLO') {
+
+    let urilocation = '';
+    localStorage.removeItem('bolsillo-transaction-status');
+    //Creamos la transaccion en el localStorage
+    const localStorageTrsnData:any = { trnStatus: false }
+    localStorage.setItem('bolsillo-transaction-status', JSON.stringify(localStorageTrsnData))
+    
+    //Propagamos la alerta
+    Swal.fire({
+      title: 'Serás redireccionado a la pagina correspondiente...',
+      html: 'Espera un momento...',
+      timer: 4000,
+      didOpen: () => {
+        Swal.showLoading();
+        this.AvalPayService.makePayment(amount,invoiceType,desc).subscribe(response =>{
+          urilocation = response.message.RefInfo[0].RefType;
+        });
+      },
+      willClose: () => {
+        // window.location.href = urilocation;
+      }
+    });
+  }
+
   recargarBolsillo(){
     this.bolsilloService.recarga(this.formValue.value,localStorage.getItem('idAcudiente')).subscribe(
       response=>{
